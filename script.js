7414eb111b49609764008b99a53d6df7bf654758
Mir-Ismayil-Asadullayev <mirim@code.edu.az>
2023-01-07 11:34:23 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let img = document.querySelector("img");
+
+setInterval(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => img.src = data.message);
+
+}, 2000)
