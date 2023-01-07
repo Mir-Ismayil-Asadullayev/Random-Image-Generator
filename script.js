6d12db70051da8af6dde647f4ba546b64f0f8897
Mir-Ismayil-Asadullayev <mirim@code.edu.az>
2023-01-07 11:40:27 +0400

    some changes

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let img = document.createElement("img");
+let box = document.querySelector(".box")
 
 setInterval(() => {
     fetch("https://dog.ceo/api/breeds/image/random")
         .then(res => res.json())
-        .then(data => img.src = data.message);
+        .then(data => {
+            img.src = data.message;
+            box.appendChild(img);
+        });
 
 }, 2000)
