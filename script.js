e6fd0dd20e3adc7ba170ced2353448be66548ab4
Mir-Ismayil-Asadullayev <mirim@code.edu.az>
2023-01-07 11:36:52 +0400

    made some changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let img = document.querySelector("img");
 
-setInterval(() => {
-    fetch("https://dog.ceo/api/breeds/image/random")
+setInterval(async () => {
+    await fetch("https://dog.ceo/api/breeds/image/random")
         .then(res => res.json())
         .then(data => img.src = data.message);
 
