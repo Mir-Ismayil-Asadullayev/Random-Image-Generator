dd7e7ba58faeee93f88637c67ac4b0969630ef9f
Mir-Ismayil-Asadullayev <mirim@code.edu.az>
2023-01-07 11:40:18 +0400

    some changes a bit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let img = document.querySelector("img");
+let img = document.createElement("img");
 
-setInterval(async () => {
-    await fetch("https://dog.ceo/api/breeds/image/random")
+setInterval(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
         .then(res => res.json())
         .then(data => img.src = data.message);
 
